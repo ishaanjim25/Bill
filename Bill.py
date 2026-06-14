9c447080f7634b4805c2e52cda0ef7bb5d91bd9a
ishaanjim25 <ishaanjim25@gmail.com>
2026-06-14 16:10:43 +0100

    Update Bill.py

--- Bill.py
+++ Bill.py
@@ -1,22 +1,11 @@
-units=int(input("Enter units consumed: "))
+def p(bill_amount, amount_amount):
+    due = bill_amount - amount_amount
+    return max(due,0)
 
-if (units<50):
-    amount=units*2.6
-    surcharge=25
+Bill=int(input("Enter the bill amount: "))
+payment=int(input("Enter the payment amount: "))
 
-elif units<=100: 
-    amount=130+ ((units-50)*3.25)
-    surcharge=35
-
-elif units<=200: 
-    amount=130+162.5+ ((units-100)*5.26)
-    surcharge=45
-
-else: 
-    amount=130+162.5+526+ ((units-200)*8.45)
-    surcharge=75
-
-total=amount+surcharge
-print("Electricity bill:",total)
+due = p(Bill,payment)
+print((f"The total to be paid is: {due}"))
 
 
